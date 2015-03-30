0445ec0833b3249817aeac06989ba34b849e50ba
rocket-raccoon <the.alberto.bena@gmail.com>
2015-03-30 16:24:32 -0400

    Modified train_retinopathy script to use SGD minibatches through big batches

--- train_retinopathy_net.py
+++ train_retinopathy_net.py
@@ -1,3 +1,11 @@
+################################################################################
+#  A big problem we have is that the data is too big to be loaded into shared  #
+#  memory.  So this script is kind of a work around to that.  Instead, what    #
+#  we do here is divide the data into "big" batches.  These batches are        #
+#  iteratively loaded into shared memory.  We can then do SGD using            #
+#  minibatches on these individual big batches.                                #
+################################################################################
+
 #Dependencies
 import os
 import sys
@@ -11,40 +19,105 @@ import settings
 from theano.ifelse import ifelse
 from theano.tensor.signal import downsample
 from theano.tensor.nnet import conv
-from logistic_sgd import LogisticRegression, load_data
+from logistic_sgd import LogisticRegression
 from mlp import HiddenLayer
 from relu import relu
 from dropout import dropout_neurons_from_layer
 from convolutional_mlp import LeNetConvPooLayer
 
-def load_batch(minibatches, index):
-    datasets = load_data(settings.PROCESSED_TRAIN_DIR + "/" + dataset)
-    train_set_x, train_set_y = datasets[0]
-    test_set_x, test_set_y = datasets[1]
+#This function loads our dataset into a shared variable for theano
+#Assumes there is (x, y) matrix pair pickled
+def load_batch(dataset):
 
+    # Load the dataset
+    f = open(dataset, 'rb')
+    x_set, y_set = cPickle.load(f)
+    f.close()
 
+    #Create shared variables out of it
+    x_set = x_set.reshape(x_set.shape[0], settings.PIXELS_PER_IMAGE)
+    shared_x = theano.shared(numpy.asarray(x_set, dtype=theano.config.floatX),
+                             borrow=True)
+    shared_y = theano.shared(numpy.asarray(y_set, dtype=theano.config.floatX),
+                             borrow=True)
+    return shared_x, T.cast(shared_y, 'int32')
 
+#This function is responsible for training our neural network
+#Every SGD step will use this to update our parameters
+def get_train_func(index, cost, updates, x, y, use_dropout,
+                   train_set_x, train_set_y, batch_size):
+    train_func = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index*train_batch_size: (index+1)*train_batch_size],
+            y: train_set_y[index*train_batch_size: (index+1)*train_batch_size],
+            use_dropout: 1.0
+        }
+    )
+    return train_func
+
+#This function will just compute the error rate on same set of data
+#We will use this to see training and test error so we can decide things like
+#when to stop training, which parameters to use, etc.
+def get_err_func(index, error_func, x, y, use_dropout,
+                 datax, datay, batch_size):
+    test_model = theano.function(
+        [index],
+        error_func,
+        givens={
+            x: datax[index*batch_size: (index + 1)*batch_size],
+            y: datay[index*batch_size: (index + 1)*batch_size],
+            use_dropout: 0.0
+        }
+    )
+    return test_model
+
+#This is a handy helper function that will return a list of all the updates
+#that need to be passed to the training function
+def get_updates(m_params, params, grads, learning_rate, momentum):
+    momentum_updates = [
+        (m_param_i, momentum * m_param_i - learning_rate * grad_i)
+        for m_param_i, grad_i in zip(m_params, grads)
+    ]
+    regular_updates = [
+        (param_i, param_i + momentum * m_param_i - learning_rate * grad_i)
+        for param_i, m_param_i, grad_i in zip(params, m_params, grads)
+    ]
+    updates = regular_updates + momentum_updates
+    return updates
+
+#This is the meat of our application
+#Will continuously train our neural network architecture until we see fit
+#to stop it.
 def train_neural_network(learning_rate = 0.05,
                          n_epochs = 200,
                          data_directory = settings.PROCESSED_TRAIN_DIR,
-                         batch_size = settings.MINI_BATCH_SIZE * settings.TRAIN_PERC,
+                         train_batch_size = 10,
+                         test_batch_size = 10,
                          nkerns = [20,50],
                          momentum = 0.9,
                          dropout_rates = [0.95, 0.75, 0.75, 0.75]):
 
-    #Due to memory constraints, we have to load the data in batches off disk
-    #So, let's get the file paths of all the minibatches in the data directory
-    minibatches = [batch for batch in os.listdir(data_directory)]
-    minibatches = [data_directory + "/" + batch for batch in minibatches]
+    #First, we load in the test batch.  This will always be in memory
+    #There should only be one in there
+    test_batch = [bb for bb in os.listdir(data_directory) of "test" in bb][0]
+    test_set_x, test_set_y = load_batch(data_directory + "/" + test_batch)
+
+    #Now, we get the paths to each of the big train batches
+    #We will load these into memory on as as need basis unlike the test batch
+    big_batches = [bb for bb in os.listdir(data_directory) if "train" in bb]
+    big_batches = [data_directory + "/" + batch for batch in minibatches]
 
     #Setup our symbolic variables for theano
     index = T.lscalar('index')
     x = T.matrix('x')
     y = T.ivector('y')
-    use_dropout = T.scalaer('use_dropout')
-    rng = numpy.random.RandomState(23455)
+    use_dropout = T.scalar('use_dropout')
 
     #Setup layer0
+    rng = numpy.random.RandomState(23455)
     layer0_input = dropout_neurons_from_layer(rng, x, dropout_rates[0], use_dropout)
     layer0_input = layer0_input.reshape((train_batch_size, 1, 512, 512))
     layer0 = LeNetConvPoolLayer(
@@ -97,68 +170,43 @@ def train_neural_network(learning_rate = 0.05,
         m_param = theano.shared(np.zeros(param_shape, dtype=data_type), borrow=True)
         m_params.append(m_param)
 
-    #Create all the updates
-    momentum_updates = [
-        (m_param_i, momentum * m_param_i - learning_rate * grad_i)
-        for m_param_i, grad_i in zip(m_params, grads)
-    ]
-    regular_updates = [
-        (param_i, param_i + momentum * m_param_i - learning_rate * grad_i)
-        for param_i, m_param_i, grad_i in zip(params, m_params, grads)
-    ]
-    updates = regular_updates + momentum_updates
+    updates = get_updates(m_params, params, grads, learning_rate, momentum)
 
-    #Create out training and testing functions
-    train_model = theano.function(
-        [index],
-        cost,
-        updates=updates,
-        givens={
-            x: train_set_x[index*train_batch_size: (index+1)*train_batch_size],
-            y: train_set_y[index*train_batch_size: (index+1)*train_batch_size],
-            use_dropout: 1.0
-        }
-    )
+    while(True):
 
-    test_model = theano.function(
-        [index],
-        layer3.errors(y),
-        givens={
-            x: test_set_x[index*test_batch_size: (index + 1)*test_batch_size],
-            y: test_set_y[index*test_batch_size: (index + 1)*test_batch_size],
-            use_dropout: 0.0
-        }
-    )
+        for big_batch in big_batches:
 
-    train_losses = theano.function(
-        [index],
-        layer3.errors(y),
-        givens={
-            x: train_set_x[index*train_batch_size: (index + 1)*train_batch_size],
-            y: train_set_y[index*train_batch_size: (index + 1)*train_batch_size],
-            use_dropout: 0.0
-        }
-    )
+            #Load in a big batch as a shared variable
+            train_set_x, train_set_y = load_batch(big_batch)
 
-    best_iter = 0
-    test_score = 0.
-    start_time = time.clock()
-    epoch = 0
+            #Setup our training and testing functions in theano
+            train_func = get_train_func(index, cost, updates, x, y,
+                                        train_set_x, train_set_y, use_dropout)
+            test_err_func = get_err_func(index, layer3.errors(y), x, y, use_dropout,
+                                         test_set_x, test_set_y, test_batch_size)
+            train_err_func = get_err_func(index, layer3.errors(y), x, y, use_dropout,
+                                          train_set_x, train_set_y, train_batch_size)
 
-    while (True):
-        epoch = epoch + 1
-        for minibatch_index in xrange(n_train_batches):
-            iter = (epoch - 1) * n_train_batches + minibatch_index
-            cost_ij = train_model(minibatch_index)
-            test_losses = [test_model(i) for i in xrange(n_test_batches)]
-            test_score = numpy.mean(test_losses)
-            train_loss = [train_losses(i) for i in xrange(n_train_batches)]
-            train_score = numpy.mean(train_loss)
-            print "Mean Test Score: %f"%(test_score * 100)
-            print "Mean Train Score: %f"%(train_score * 100)
+            #Run SGD over the big batch
+            for minibatch_index in xrange(n_train_batches):
+
+                #Take one training step
+                minibatch_avg_cost = train_func(minibatch_index)
+                itr += 1
+
+                #Compute the test error
+                test_losses = [test_err_func(i) for i in xrange(n_test_batches)]
+                test_score = numpy.mean(test_losses)
+                print "Mean Test Error: %f"%(test_score * 100)
+
+                #Compute the training error
+                train_loss = [train_err_func(i) for i in xrange(n_train_batches)]
+                train_score = numpy.mean(train_loss)
+                print "Mean Train Error: %f"%(train_score*100)
+
+        epoch += 1
         if epoch > n_epochs:
             break
-
 
 if __name__ == "__main__":
     train_retinopathy_net()
